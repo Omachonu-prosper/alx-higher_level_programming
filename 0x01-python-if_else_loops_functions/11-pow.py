96f70414f1d162d59f343cbd2917c45e4ce8c92b
Omachonu Prosper <omachonucodes@gmail.com>
2023-03-08 04:53:17 -0800

    a ^ b

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -6,7 +6,10 @@ def pow(a, b):
     if b == 0:
         return 1
 
-    for i in range(0, b - 1):
+    for i in range(0, abs(b) - 1):
         current_value *= a
+
+    if b < 0:
+        return (1 / current_value)
         
     return current_value
