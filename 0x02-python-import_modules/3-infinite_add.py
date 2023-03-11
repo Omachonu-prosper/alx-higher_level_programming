798ee7806275c85d05f101cd31ab1d853fd6b840
Omachonu Prosper <omachonucodes@gmail.com>
2023-03-10 23:51:57 -0800

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+
+def infinite_add():
+    result_of_addition = 0
+
+    for i in range(1, len(sys.argv)):
+        result_of_addition += int(sys.argv[i])
+
+    print(result_of_addition)
+
+
+if __name__ == "__main__":
+    infinite_add()
