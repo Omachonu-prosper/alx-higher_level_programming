c2e8d814d329b3717190529182fb4ef715a49b3f
Omachonu Prosper <omachonucodes@gmail.com>
2023-04-06 11:31:39 -0700

    Fix - Width and height values must be up to specifications

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,9 +12,10 @@ class Rectangle:
     """Rectangle class which defines properties for a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Constructor function for this class.
+        """Constructor function for this class."""
+        self.checkWidth(width)
+        self.checkHeight(height)
 
-        Instantiates private attributes width and height to 0"""
         self.__width = width
         self.__height = height
 
@@ -26,27 +27,30 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Setter for the width attribute"""
-        if type(value) != int:
-            raise TypeError('width must be an integer')
-
-        if value < 0:
-            raise ValueError('width must be >= 0')
-
+        self.checkWidth(value)
         self.__width = value
         return
 
     @property
     def height(self):
-        """Getter for the height property."""
+        """Getter for the height attribute."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
-            raise TypeError('height must be an integer')
-
-        if value < 0:
-            raise ValueError('height must be >= 0')
-
+        """Setter for the height attribute."""
+        self.checkHeight(value)
         self.__height = value
         return
+
+    def checkWidth(self, width):
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+
+    def checkHeight(self, height):
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
