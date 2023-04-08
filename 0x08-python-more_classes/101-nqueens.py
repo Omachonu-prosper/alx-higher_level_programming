a95a89a7be1dd5b2bfe393750cd025afe73e40d7
Omachonu Prosper <omachonucodes@gmail.com>
2023-04-08 03:47:22 +0100

    N Queens problem

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def nqueens(n):
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    def is_valid(board, row, col):
+        for i in range(row):
+            if board[i] == col or \
+               board[i] - i == col - row or \
+               board[i] + i == col + row:
+                return False
+        return True
+
+    def solve(board, row):
+        result = []
+        if row == n:
+            for i in range(n):
+                result.append([i, board[i]])
+            print(result)
+
+            return
+        for col in range(n):
+            if is_valid(board, row, col):
+                board[row] = col
+                solve(board, row + 1)
+
+    solve([None] * n, 0)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    nqueens(n)
