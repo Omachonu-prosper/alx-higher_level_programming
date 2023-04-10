fb3278d19b7fa1ff1707c73a3575163a874eb290
Omachonu Prosper <omachonucodes@gmail.com>
2023-04-10 02:28:48 +0100

    Say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""The function that prints My name is <first name> <last name>
+
+First name and last name must be strings
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints out given name in specific format.
+    
+    First name and last name must be strings
+    
+    Example input:
+    >>> say_my_name("John", "Smith")
+    
+    Example output:
+    My name is John Smith
+    """
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+    if last_name:
+        last_name = ' ' + last_name
+
+    print("My name is {}{}".format(first_name, last_name))
