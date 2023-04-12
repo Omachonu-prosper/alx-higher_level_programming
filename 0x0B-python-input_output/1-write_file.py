b2de98511d2f3aed2bf76668305336456e5623be
Omachonu Prosper <omachonucodes@gmail.com>
2023-04-12 23:28:40 +0100

    Fix - pycodestyle line length error

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,6 @@
 
 
 def write_file(filename="", text=""):
-    """function that writes to a file and returns the number of chars writtten."""
+    """writes to a file and returns the number of chars writtten."""
     with open(filename, 'w') as f:
         return f.write(text)
