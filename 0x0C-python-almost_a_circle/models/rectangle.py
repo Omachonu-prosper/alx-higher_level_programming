d4e2e88fe548fb84f4df6780dd1ea0442751ea98
Omachonu Prosper <omachonucodes@gmail.com>
2023-04-20 16:44:15 +0100

    Overide __str__ | prints out rectangle instance description

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,6 +20,15 @@ class Rectangle(Base):
         self.__y = y
         super().__init__(id)
 
+    def __str__(self):
+        return '[Rectangle] ({}) {}/{} - {}/{}'.format(
+            self.id,
+            self.__x,
+            self.__y,
+            self.__width,
+            self.__height
+        )
+
     @property
     def width(self):
         return self.__width
