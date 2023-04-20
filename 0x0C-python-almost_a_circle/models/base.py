a90d9b5f8bd8b3f67dd5aefbaaa065bdb154a8c7
Omachonu Prosper <omachonucodes@gmail.com>
2023-04-20 13:55:57 +0100

    Create unique ids for new objects

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Base module."""
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is None:
+            self.id = self.__nb_objects + 1
+        else:
+            self.id = id
