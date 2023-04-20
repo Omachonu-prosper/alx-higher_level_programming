3a606b8acd2299e81265f25bfea88defbac2da14
Omachonu Prosper <omachonucodes@gmail.com>
2023-04-20 22:26:59 +0100

    Update a rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,6 +80,22 @@ class Rectangle(Base):
         for i in range(height):
             print(' ' * x + '#' * width)
 
+    def update(self, *args):
+        if len(args) > 0:
+            self.id = args[0]
+
+        if len(args) > 1:
+            self.__width = args[1]
+
+        if len(args) > 2:
+            self.__height = args[2]
+
+        if len(args) > 3:
+            self.__x = args[3]
+
+        if len(args) > 4:
+            self.__y = args[4]
+
     @staticmethod
     def validateValue(value, value_type):
         """Validates the values before instantiation and for setters.
