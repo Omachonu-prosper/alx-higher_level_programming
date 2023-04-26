948d21947afcadd49a2ff91c075c53b6c5a77bf0
Omachonu Prosper <omachonucodes@gmail.com>
2023-04-26 17:55:23 +0100

    Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Text indentation"""
+
+
+def text_indentation(text):
+    """Text indentition function
+    prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+
+    for i in range(len(text)):
+        if text[i] in ['.', '?', ':']:
+            print('{}\n'.format(text[i]))
+        elif not (text[i] == ' ' and text[i - 1] in ['.', '?', ':']):
+            print(text[i], end='')
