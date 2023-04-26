3d21a6375aef2e1fd592618b8bb0d8ddec82114e
Omachonu Prosper <omachonucodes@gmail.com>
2023-04-26 18:45:23 +0100

    Braoder test coverage

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -16,6 +16,36 @@ class TestMaxInteger(unittest.TestCase):
         max = max_integer(arr)
         self.assertEqual(max, 1)
 
+    def test_one_element(self):
+        arr = [3]
+        max = max_integer(arr)
+        self.assertEqual(max, 3)
+
+    def test_one_negative_number(self):
+        arr = [1, 2, -3, 4, 5]
+        max = max_integer(arr)
+        self.assertEqual(max, 5)
+
+    def test_max_at_end(self):
+        arr = [4, 5, 3, 2, 10]
+        max = max_integer(arr)
+        self.assertEqual(max, 10)
+
+    def test_max_at_beginning(self):
+        arr = [10, 4, 5, 6]
+        max = max_integer(arr)
+        self.assertEqual(max, 10)
+
+    def test_max_at_mid(self):
+        arr = [1, 2, 10, 4, 5]
+        max = max_integer(arr)
+        self.assertEqual(max, 10)
+
+    def test_negative_only(self):
+        arr = [-1, -2, -3, -4, -5]
+        max = max_integer(arr)
+        self.assertEqual(max, -1)
+
     def test_mixed_array(self):
         arr = [1, 'h', 3, 5]
 
