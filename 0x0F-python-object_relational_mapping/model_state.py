a6dfa71b452df1739e1b4a6dd0ff0553ba355d87
Omachonu Prosper <omachonucodes@gmail.com>
2023-06-20 10:25:27 +0100

    Introduce sqlalchemy to the workspace

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""contains the class definition of a State and an instance
+Base = declarative_base()
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
+
+
+Base = declarative_base()
+
+class State(Base):
+	"""User class representing the users table
+	"""
+	__tablename__ = 'states'
+
+	id = Column('id', Integer, primary_key=True)
+	name = Column('name', String(128), nullable=False)
