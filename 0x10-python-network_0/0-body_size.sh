3d2052340f1bb5f48c7d954d69bc4dcc45e72e13
Omachonu Prosper <omachonucodes@gmail.com>
2023-06-29 02:25:27 -0700

    Make the script only 3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-# Get the URL from the user
-url=$1
-
-# Send a request to the URL
-response=$(curl -Is $url)
-
-# Get the size of the response body
-body_size=$(echo $response | grep -Eo 'Content-Length: [0-9]+' | cut -d ':' -f 2)
-
-# Display the size of the response body
-echo $body_size
-
+curl -sI $1 | grep 'Content-Length' | cut -d ':' -f 2 | tr -d ' '
