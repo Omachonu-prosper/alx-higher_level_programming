bf685c156ec2ba3f478536d7e9fd2b2e5685f5cf
Omachonu Prosper <omachonucodes@gmail.com>
2023-07-06 07:13:23 -0700

    Fix type in response/responses

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,7 @@ def fetch():
     """
     with re.urlopen('https://alx-intranet.hbtn.io/status') as res:
         html = res.read()
-        print('Body responses:')
+        print('Body response:')
         print(f"\t- type: {type(html)}")
         print(f"\t- content: {html}")
         print(f"\t- utf8 content: {html.decode('utf-8')}")
