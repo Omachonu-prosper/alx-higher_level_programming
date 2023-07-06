05eb1eea0f833e00eb8564da429c9e7aec27380b
Omachonu Prosper <omachonucodes@gmail.com>
2023-07-06 15:05:29 +0100

    Fetch https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module to fetche https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request as re
+
+
+def fetch():
+    """ Function to fetche https://alx-intranet.hbtn.io/status
+    """
+    with re.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        html = res.read()
+        print('Body responses:')
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")
+
+
+if __name__ == '__main__':
+    fetch()
