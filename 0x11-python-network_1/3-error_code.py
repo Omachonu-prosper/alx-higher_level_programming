e209199827a5f50ab1f404346cd19353f0c5a932
Omachonu Prosper <omachonucodes@gmail.com>
2023-07-06 08:31:52 -0700

    Print Error Codes when they arrise

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Python script that:
+    takes in a URL
+    sends a request to the URL
+    displays the body of the response (decoded in utf-8)
+"""
+
+import urllib.request as re
+import urllib.error as err
+import sys
+
+
+def main(url):
+    """Entry point
+    """
+    request = re.Request(url)
+    try:
+        with re.urlopen(request) as res:
+            print(res.read().decode('utf-8'))
+    except err.HTTPError as e:
+        print(f"Error code: {e.code}")        
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    main(url)
