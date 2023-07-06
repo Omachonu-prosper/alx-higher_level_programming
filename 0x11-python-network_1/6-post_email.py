826beb9d59277bf6a12838408e0e24cb384c1868
Omachonu Prosper <omachonucodes@gmail.com>
2023-07-06 17:44:15 +0100

    Fix pycodestyle errors

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Python script that:
-	takes in a URL and an email address
-	sends a POST request to the passed URL with the email as a parameter
-	finally displays the body of the response.
+    takes in a URL and an email address
+    sends a POST request to the passed URL with the email as a parameter
+    finally displays the body of the response.
 """
 
 import sys
@@ -10,11 +10,11 @@ import requests
 
 
 def main(url, email):
-	req = requests.post(url, data={'email': email})
-	print(req.text)
+    req = requests.post(url, data={'email': email})
+    print(req.text)
 
 
 if __name__ == '__main__':
-	url = sys.argv[1]
-	email = sys.argv[2]
-	main(url, email)
+    url = sys.argv[1]
+    email = sys.argv[2]
+    main(url, email)
