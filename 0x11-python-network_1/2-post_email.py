b50285fba47d33655722430863f8461e9c4a4294
Omachonu Prosper <omachonucodes@gmail.com>
2023-07-06 08:10:35 -0700

    Send email in POSt request using urllib

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,16 +6,18 @@ displays the body of the response (decoded in utf-8)
 """
 
 import urllib.request as re
+import urllib.parse
 import sys
 
 
 def main(url, email):
     """Entry Point
     """
-    request = re.Request(url, email=email)
-    request.method = 'POST'
-    with re.urlopen(url) as response:
-        print(response)
+    data = {'email': email}
+    data = urllib.parse.urlencode(data).encode("ascii")
+    request = re.Request(url, data)
+    with re.urlopen(request) as response:
+        print(response.read().decode('utf-8'))
 
 
 if __name__ == '__main__':
